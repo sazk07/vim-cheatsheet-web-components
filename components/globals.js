dcde87c773538dd106d969cb53788d3a792aaae7
Shahan Arshad <68821506+sazk07@users.noreply.github.com>
2024-02-17 15:13:21 +0500

    Initial commit

---
+++ components/globals.js
@@ -0,0 +1,9 @@
+import en_us from "../data/en_us.json"
+const globalsTemplate = document.createElement("template")
+globalsTemplate.innerHTML = `hello`
+
+class Globs extends HTMLElement {
+    constructor() {
+        super()
+    }
+}
