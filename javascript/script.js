aee09bf82fc3889c361df8f034c274cd00b224fe
Shahan Arshad <68821506+sazk07@users.noreply.github.com>
2024-02-24 20:01:22 +0500

    feat: Add htmlId field to data

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const fetchData = async (url) => {
   try {
     const dataPromise = await fetch(url);
@@ -12,42 +14,36 @@ const fetchData = async (url) => {
   }
 };
 
-const createCustomHeadings = (data, selectedHeadings, headings) => {
-  headings.forEach((componentHeading, index) => {
-    componentHeading.textContent = data[selectedHeadings[index]]?.title ?? "";
+const createCustomHeadings = (data, kw, heading) => {
+  heading.textContent = data[kw]?.title ?? "";
+};
+
+const createCustomLists = (commandsList, vcUl) => {
+  commandsList.forEach((elem) => {
+    const { keyword, additional_keyword2, additional_keyword3, description } =
+      elem;
+    const kbdArray = ["<kbd>", "</kbd>"];
+    const sanitizeKeyword = (str) => str?.replace(/</g, "&lt;");
+    const keywords = [keyword, additional_keyword2, additional_keyword3]
+      ?.map(sanitizeKeyword)
+      .filter(Boolean);
+    const keywordString = keywords
+      .map((kw, i) => {
+        const prefix = i === 0 ? "" : " or ";
+        return `${prefix}${kbdArray[0]}${kw}${kbdArray[1]}`;
+      })
+      .join("");
+    const li = document.createElement("li");
+    li.innerHTML = `${keywordString} - ${description}`;
+    vcUl.appendChild(li);
   });
 };
 
-const createCustomLists = (commandsList, listNodes) => {
-  listNodes.forEach((componentCommand, index) => {
-    commandsList[index].forEach((elem) => {
-      const { keyword, additional_keyword2, additional_keyword3, description } =
-        elem;
-      const kbdArray = ["<kbd>", "</kbd>"];
-      const sanitizeKeyword = (str) => str?.replace(/</g, "&lt;");
-      const keywords = [keyword, additional_keyword2, additional_keyword3]
-        ?.map(sanitizeKeyword)
-        .filter(Boolean);
-      const keywordString = keywords
-        .map((kw, i) => {
-          const prefix = i === 0 ? "" : " or ";
-          return `${prefix}${kbdArray[0]}${kw}${kbdArray[1]}`;
-        })
-        .join("");
-      const li = document.createElement("li");
-      li.innerHTML = `${keywordString} - ${description}`;
-      componentCommand.appendChild(li);
-    });
-  });
-};
-
-const createCustomTips = (data, selectedHeadings, tips) => {
-  tips.forEach((componentTip, index) => {
-    componentTip.innerHTML = data[selectedHeadings[index]]?.tip ?? "";
-    if (!componentTip.innerHTML) {
-      componentTip.style.display = "none";
-    }
-  });
+const createCustomTips = (data, kw, tip) => {
+  tip.innerHTML = data[kw]?.tip ?? "";
+  if (!tip.innerHTML) {
+    tip.style.display = "none";
+  }
 };
 
 const main = async () => {
@@ -68,21 +64,25 @@ const main = async () => {
     );
   });
 
-  // Headings
-  const customComponentHeadings = document.querySelectorAll(".heading");
-  createCustomHeadings(data, selectedKeys, customComponentHeadings);
+  const vimComponentIds = Array.from(
+    document.querySelectorAll("vim-component"),
+    (vimComponent) => vimComponent.id,
+  );
+  const kwIdObj = selectedKeys.reduce(
+    (acc, key, i) => ({ ...acc, [key]: vimComponentIds[i] }),
+    {},
+  );
 
-  // Lists
-  const customComponentLists = document.querySelectorAll(".list");
-  const commandsList = selectedKeys.map((relevantSection) => {
-    return Object.values(data[relevantSection]?.commands);
-  });
-  createCustomLists(commandsList, customComponentLists);
-
-  // Tips
-  const customComponentTips = document.querySelectorAll(".tip");
-  createCustomTips(data, selectedKeys, customComponentTips);
-
+  // Headings, lists and tips
+  for (const [kw, val] of Object.entries(kwIdObj)) {
+    const vimComponent = document.querySelector(`#${val}`);
+    const vcHeading = vimComponent.querySelector("[name='heading']");
+    createCustomHeadings(data, kw, vcHeading);
+    const vcUl = vimComponent.querySelector("[name='list']");
+    createCustomLists(data[kw]?.commands, vcUl);
+    const vcTip = vimComponent.querySelector("[name='tip']");
+    createCustomTips(data, kw, vcTip);
+  }
   // Footer
   const footer = document.querySelector("footer");
   const anchor = document.createElement("a");
